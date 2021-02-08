f2140531db5969ca07e5e5baca24f4fe923b3ff7
Kevin Kruelle <kkruelle2@gmail.com>
2021-02-08 15:28:04 -0500

    add html/js files

---
+++ js/hangman.js
@@ -0,0 +1,12 @@
+console.log('connected...')
+
+/*
+1. Create an array of possible words to play hangman with.
+2. Determine word from array to play game with.
+3. Have computer determine how many tries user has before user loses.
+4. To begin game, prompt user to select a letter of the alphabet.
+5. If the letter user selected is not contained in the word selected from the array to play, give user a tally, and prompt user to guess again.
+6. If the letter is contained in the word selected, reveal letter and ask user to select another letter.
+7. If user doesn't get the word in as many tries as was determined, alert User lost!
+8.If user gets the word in under the amount of tries determined, User Won!
+*/
